1ffc6811580699c5aaaca60d74473c3c2db54824
mihir <54289205+mihir1709@users.noreply.github.com>
2023-02-22 00:35:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://127.0.0.1/AlienDBex'
+const app = express()
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open',()=> {
+    console.log('connected...')
+
+})
+app.use(express.json())
+const alienRouter=require('./routes/aliens')
+app.use('/aliens',alienRouter)
+app.listen(5000,()=>{
+    console.log('server started')
+})
